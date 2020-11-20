f331510672102b989cdd8411512b20d989e4ed53
Mohamed Dabbour <ortiiga18@Samis-MacBook-Air.local>
2020-11-20 18:44:17 +0200

    initial xcode project creation

---
+++ youtube-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  youtube-app
+//
+//  Created by Mohamed Dabbour on 11/20/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
