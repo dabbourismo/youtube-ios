979f84f518659661311662213c01f01b11d9e8ba
Mohamed Dabbour <ortiiga18@Samis-MacBook-Air.local>
2020-11-20 20:45:48 +0200

    Adding Api testing

---
+++ youtube-app/Models/Video.swift
@@ -0,0 +1,15 @@
+//
+//  Video.swift
+//  youtube-app
+//
+//  Created by Mohamed Dabbour on 11/20/20.
+//
+
+import Foundation
+struct Video {
+    var videoId = ""
+    var title = ""
+    var description = ""
+    var thumbnail = ""
+    var published = ""
+}
---
+++ youtube-app/Services/ApiService.swift
@@ -0,0 +1,8 @@
+//
+//  ApiService.swift
+//  youtube-app
+//
+//  Created by Mohamed Dabbour on 11/20/20.
+//
+
+import Foundation
